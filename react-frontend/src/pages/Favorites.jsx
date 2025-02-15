2acf99d13463d4e842920d3f37abb051536d0c84
Yotsawat <y.zesshi.l@gmail.com>
2025-02-15 15:55:19 +0700

    finish

--- react-frontend/src/pages/Favorites.jsx
+++ react-frontend/src/pages/Favorites.jsx
@@ -1,8 +1,27 @@
-function Favorites(){
- 
- return <div>
-  <h1>  asddsa </h1>
- </div>
+import "../css/Favorites.css"
+import { useMovieContext } from "../contexts/MovieContext";
+import MovieCard from "../components/MovieCard";
+
+function Favorites() {
+    const { favorites } = useMovieContext();
+    if (favorites) {
+        return (
+            <div className="favorites">
+            <h2>Your Favorites</h2>
+            <div className="movies-grid">
+                    {favorites.map((movie) => (
+                      <MovieCard key={movie.id} movie={movie} />
+                      ))}
+                </div>
+        </div>
+        )
+    }
+
+
+    return <div className="favorites-empty">
+        <h2>Your Favorites List is Empty</h2>
+        <p>Find your favorite movies and add them to your favorites list.</p>
+    </div>
 }
 
 export default Favorites;
--- react-frontend/src/pages/Home.jsx
+++ react-frontend/src/pages/Home.jsx
@@ -1,42 +1,73 @@
 import MovieCard from "../components/MovieCard"
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { searchMovies, getPopularMovies } from "../services/api"
+import "../css/Home.css"
 
-function Home(){
+function Home() {
 
     const [searchQuery, setSearchQuery] = useState('')
+    const [movies, setMovies] = useState([]);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true)
 
-    const movies = [
-        {id: 1, title: 'The Shawshank Redemption', release_date: '1994', url: 'https://images.unsplash.com/photo-1612833943300-4b3b3e3b3b3b'},
-        {id: 2, title: 'The Godfather', release_date: '1972', url: 'https://images.unsplash.com/photo-1612833943300-4b3b3e3b3b3b'},
-        {id: 3, title: 'The Dark Knight', release_date: '2008', url: 'https://images.unsplash.com/photo-1612833943300-4b3b3e3b3b3b'},
-    ]
+    useEffect(() => {
+        const loadPopularMovies = async () => {
+            try {
+                const popularMovies = await getPopularMovies()
+                setMovies(popularMovies)
+            } catch (err) {
+                console.log(err)
+                setError("Fail to load movies...")
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+        loadPopularMovies()
+    }, [])
 
-    const handleSearch = (e) => {
+
+    const handleSearch = async(e) => {
         e.preventDefault()
-        alert(`Searching for ${searchQuery}`)
-        setSearchQuery('')
+        if(!searchQuery.trim()) return
+        if (loading) return
+        setLoading(true)
+        try{
+            const searchResults = await searchMovies(searchQuery)
+            setMovies(searchResults)
+            setError(null)
+        }catch (err){
+            console.log(err)
+            setError("Failed to search movies...")
+        }finally{
+            setLoading(false)
+        }
     }
 
     return (
-        
+
         <div className="home">
             <form onSubmit={handleSearch} className="search-from" >
-                <input type="text" 
-                placeholder="Search for movies..." 
-                className="search-input" 
-                value={searchQuery}
-                onChange = {(e) => setSearchQuery(e.target.value)}
+                <input type="text"
+                    placeholder="Search for movies..."
+                    className="search-input"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
                 />
                 <button type="submit" className="search-button">
                     Search
                 </button>
             </form>
-            <div className="movie-grid">
-                {movies.map((movie) => (
-                    movie.title.toLowerCase().startsWith(searchQuery) &&
-                    (<MovieCard key={movie.id} movie={movie} />)
-                ))}
-            </div>
+
+            {error && <div className="error-message">{error}</div> }
+
+            {loading ? <div className="loading">Loading...</div>
+                : <div className="movies-grid">
+                    {movies.map((movie) => (
+                      <MovieCard key={movie.id} movie={movie} />
+                      ))}
+                </div>
+            }
         </div>
     )
 
